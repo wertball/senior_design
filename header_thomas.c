476fd75e6ea227d3eec33301886145d89fec70a1
Class Account - Auburn <aubcls17@uv.asc.edu>
2018-02-14 18:05:46 -0600

    Version 4.0 Program currently works great with decimals between 0.0 and 1.0 Currently biases are turned off. If you define a variable called BIAS, they will be turned on. Similarly, if you make a variable called DEBUG_MODE, the values of the weights and the hidden node outputs will be printed.

--- header_thomas.c
+++ header_thomas.c
@@ -5,86 +5,110 @@
 #include <sys/time.h>
 
 main(){
+
 	float error, last, average, error_one, error_two;
 	int i, j, k, kk;
-	float input[] = {1, 2, 5, 6};
-	float output[] = {1, 4, 25, 36};
-	float input_bit[4];
-	float output_bit[4];
-	for(i = 0; i < 4; i++){
-		input_bit[i] = input[i] / 256.0;
-		output_bit[i] = output[i] / 256.0;
-	}
+	//float input[] = {1, 2, 5, 6};
+	//float output[] = {1, 4, 25, 36};
+	//float input_bit[4];
+	//float output_bit[4];
+	//for(i = 0; i < 4; i++){
+	//	input_bit[i] = input[i] / 256.0;
+	//	output_bit[i] = output[i] / 256.0;
+	//}
 	error_one = 0;
+
 	initialize();
+
+#ifdef DEBUG_MODE
 	printf("The initial weights are: \n");
 	for(k = 0; k < L-1; k++){
 		for(j = 0; j < H; j++)
 			printf("%f  ",weights[j][k]);
 	printf("\n");
 	}
-	//j = 2;
-	//for(j = 0; j < 4; j++){
+
+#ifdef BIAS
+	printf("The initial biases are: \n");
+	for(k = 0; k < H; k++)
+		printf("%f ",bias[k]);
+	printf("\n");
+#endif
+#endif
+
+
 	for(i = 0; i < 1000000; i++){
-		for(j = 0; j < 4; j++){
-			error = train_network(input[j], output[j]);
+		for(j = 0; j < training_set_size; j++){
+			error = train_network(training_set_input[j], training_set_target[j]);
 			error_one += error;
 		}
-		if((error_one / 4) < .000001)
+		if((error_one / training_set_size) < .000001)
 			break;
-//		printf("The total error is %f after %d runs\n", error, i);	
-//		printf("\n");
-//		last = (feed_fwd(input[j]));
-//		printf("With an input of %f, the output is %f, the desired output is %f, the difference is %f\n",input[j],last,output[j],last-output[j]);
-//		printf("The weights after run %d are: \n", (j+1));
-//		for(k = 0; k < L-1; k++){
-//			for(i = 0; i < H; i++){
-//				printf("%f  ",weights[i][k]);
-//				weights_one[i][k] = weights_one[i][k] + weights[i][k];
-//			}
-//		printf("\n");
-//		}
 		error_one = 0;
 	}
-	printf("The total error is %f after %d runs\n", error, i);	
-//	for(k = 0; k < L-1; k++){
-//		for(i = 0; i < H; i++){
-//			weights[i][k] = weights_one[i][k] / 4;
-//		}
-//	}
-	for(j = 0; j < 4; j++){
-		last = (feed_fwd(input[j]));
-		printf("With an input of %f, the output is %f, the desired output is %f, the difference is %f\n",input[j],last,output[j],last-output[j]);
+
+	printf("The total error is %f after %d runs\n", error, i);
+
+	for(j = 0; j < training_set_size; j++){
+		last = (feed_fwd(training_set_input[j]));
+		printf("With an input of %f, the output is %f, the desired output is %f, the difference is %f\n",training_set_input[j],last,training_set_target[j],last-training_set_target[j]);
+#ifdef DEBUG_MODE
+		printf("The outputs are: \n");
+		for(k = 0; k < H; k++){
+			printf("%f ", h_o[k][0]);
+		}
+		printf("\n");
+#endif
 	}
+
+	printf("\n");
+	error_one = 0;
+	for(j = 0; j < testing_set_size; j++){
+		last = (feed_fwd(testing_set_input[j]));
+		error = find_total_error(testing_set_target[j], last);
+		printf("With an input of %f, the output is %f, the desired output is %f, the difference is %f\n",testing_set_input[j],last,testing_set_target[j],last - testing_set_target[j]);
+		error_one += error;
+	}
+	printf("The total error of the testing set is %f\n", error_one/testing_set_size);
+
+#ifdef DEBUG_MODE
 	printf("The final weights are: \n");
 	for(k = 0; k < L-1; k++){
 		for(j = 0; j < H; j++)
 			printf("%f  ",weights[j][k]);
 	printf("\n");
 	}
-//	printf("The total error is %f after %d runs\n", error, i);	
-//	printf("\n");
-	//j = 2;
-//	for(j = 0; j < 4; j++){
-//		last = (feed_fwd(input[j]));
-//		printf("With an input of %f, the output is %f, the desired output is %f, the difference is %f\n",input[j],last,output[j],last-output[j]);
-//	}
+
+#ifdef BIAS
+	printf("The final biases are: \n");
+	for(k = 0; k < H; k++)
+		printf("%f ",bias[k]);
+	printf("\n");
+#endif
+#endif
+
 }
 
 void initialize(){
 	int i, j;
 	int r;
+
 	srand(time(NULL));
+
 	for(i = 0; i < H; i++){
 		for(j = 0; j < L-1; j++){
 			r = rand() % 4;
-			weights[i][j] = (float) r + j + 1.0;
-			//weights[i][j] = i * j + 1.0;
+			weights[i][j] = (float) r + 1;
 			weights_new[i][j] = 0.0;
-			if(j < L-2)
+			if(j < L-2){
 				h_o[i][j] = 0.0;
+#ifdef BIAS
+				bias[i] = (float) r + 2;
+#endif
+			}
 		}
 	}
+
 }
 
 float feed_fwd(float x){
@@ -92,27 +116,36 @@ float feed_fwd(float x){
 	int i;
 	float input, sum, output;
 	input = x;
+
 	//input -> hidden layer
 	for(i = 0; i < H; i++)
-		h_o[i][0] = activate(input * weights[i][0]);
+		h_o[i][0] = activate(input * weights[i][0] 
+#ifdef BIAS
+								+ bias[i]
+#endif
+							);
 	
 	//hidden -> output layer
 	sum = 0;
 	for(i = 0; i < H; i++)
 		sum = sum + h_o[i][0] * weights[i][1];
 	output = sum;
+
 	//large_output = output * 256.0;
 	return output;
+
 }
 
 float train_network(float input, float output){
+
   	//feed_fwd
   	int i, j;
-	float answer, dw, total_error, desired;
+	float answer, dw, total_error, desired, bw, db;
 	answer = feed_fwd(input);
-	//desired = output / 256;
+
   	//find_total_error
-	total_error = find_total_error((output), (answer));
+	total_error = find_total_error(output, answer);
+
   	//backpropagate
 	for(i = L-2; i > -1 ; i--){
 		for(j = 0; j < H; j++){
@@ -127,12 +160,23 @@ float train_network(float input, float output){
 		}
 		//printf(".\n");
 	}
+#ifdef BIAS
+	for(j = 0; j < H; j++){
+		bw = bias[j];
+		db = (answer - output) * weights[j][1] * h_o[j][0] * (1 - h_o[j][0]);
+		bw = bw - db * c;
+		bias[j] = bw;
+	}
+#endif
+
 	//implement new weights
 	for(i = 0; i < L-1; i++){
 		for(j = 0; j < H; j++)
 			weights[j][i] = weights_new[j][i];
 	}
+
 	return total_error;
+
 }
 
 float activate(float x){
--- header_thomas.h
+++ header_thomas.h
@@ -3,14 +3,26 @@
 
 #define c .25 //learning constant
 #define I 1 //number of input layer nodes
-#define H 3 //number of nodes per hidden layer
+#define H 10 //number of nodes per hidden layer
 #define O 1 //number of output layer nodes
 #define L 3 //number of layers
+#define training_set_size 4 //size of training set
+#define testing_set_size 4 //size of testing set
 
 float weights[H][L-1]; //weights matrix
 float weights_new[H][L-1]; //new weights calculated by matrix
 float weights_one[H][L-1];
 float h_o[H][L-2]; //hidden layer outputs
+
+#ifdef BIAS
+float bias[H];
+#endif
+
+float training_set_input[training_set_size] = {.1,.3,.7,.9};
+float training_set_target[training_set_size] = {.01,.09,.49,.81};
+
+float testing_set_input[testing_set_size] = {.2,.4,.6,.8};
+float testing_set_target[testing_set_size] = {.04,.16,.36,.64};
 
 float feed_fwd(float x);
 float deactivate(float y);
