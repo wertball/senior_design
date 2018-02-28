ab39e8a7ba8eb927eee66af0f718347f7c52ef4a
Evan <etk0001@auburn.edu>
2018-02-27 23:14:42 -0600

    Multiple layers initial commit.

--- neural_network_evan.c
+++ neural_network_evan.c
@@ -5,36 +5,36 @@
 
 #define INPUTS 1
 #define H_LAYERS 1
-#define H_HEIGHT 20
+#define H_HEIGHT 10
 #define OUTPUTS 1
 #define BIAS 1
 
-#define DATA_MIN 2
-#define DATA_MAX 121
+#define DATA_MIN 0
+#define DATA_MAX 256
 #define DATA_RANGE (DATA_MAX - DATA_MIN)
 
 //Weight declarations
-float weights_in[INPUTS * H_HEIGHT];// = {.15,.20,.25,.30};
-float weights_out[OUTPUTS * H_HEIGHT];// = {.65,.7,.75,.8};
+double weights_in[INPUTS * H_HEIGHT];// = {.15,.20};
+double weights_out[OUTPUTS * H_HEIGHT];// = {.65,.7};
 #if (BIAS > 0)
-float weights_bias_out[OUTPUTS];// = {0.85, 0.85};
-float weights_bias_h[H_LAYERS][H_HEIGHT];// = {{.35,.35}, {.6,.6}};
+double weights_bias_out[OUTPUTS];// = {0.85};
+double weights_bias_h[H_LAYERS][H_HEIGHT];// = {{.3,.35}, {.6,.75}};
 #endif
 #if (H_LAYERS > 1)
-float weights_h[H_LAYERS - 1][H_HEIGHT * H_HEIGHT] = {.4,.45,.5,.55};
+double weights_h[H_LAYERS - 1][H_HEIGHT * H_HEIGHT];// = {.4,.45,.5,.55};
 #endif
 
 //I/O declarations
-float h_out[H_LAYERS][H_HEIGHT];
-float inputs[INPUTS]; //= {0.05, 0.10};
-float outputs[OUTPUTS];
+double h_out[H_LAYERS][H_HEIGHT];
+double outputs[OUTPUTS];
 
 //Learning declarations
-float ALPHA = 0.5;
+double ALPHA = 0.5;
 
 //Randomization functions----------------------
-void randomize_array(int length, float arr[length]) {
-    for (int i = 0; i < length; i++) {
+void randomize_array(int length, double arr[length]) {
+    int i;
+    for (i = 0; i < length; i++) {
         arr[i] = ((rand() & 0b11) + 1) / ((rand() & 0xF) + 1);
         if ((rand() & 1) == 0) {
             arr[i] *= -1;
@@ -42,8 +42,9 @@ void randomize_array(int length, float arr[length]) {
     }
 }
 
-void randomize_2d_array(int rows, int columns, float arr[rows][columns]) {
-    for (int i = 0; i < rows; i++) {
+void randomize_2d_array(int rows, int columns, double arr[rows][columns]) {
+    int i;
+    for (i = 0; i < rows; i++) {
         randomize_array(columns, arr[i]);
     }
 }
@@ -65,78 +66,80 @@ static inline void initialize() {
 }
 
 //Sigmoid activation function
-static inline float activation(float input) {
+static inline double activation(double input) {
     return 1.0f / (1.0f + exp(-1 * input));
 }
 
 //Normalization functions--------------------
-static inline float normalize(float input) {
+static inline double normalize(double input) {
     return (input - DATA_MIN) / (DATA_RANGE);
 }
 
-static inline float denormalize(float input) {
+static inline double denormalize(double input) {
     return (DATA_RANGE) * input + DATA_MIN;
 }
 //--------------------------------------------
 
-void forward(float in[INPUTS]) {
+void forward(double in[INPUTS]) {
+    int i,j,k;
+
     //Input layer-----------------------
-    for (int i = 0; i < H_HEIGHT; i++) {
+    for (i = 0; i < H_HEIGHT; i++) {
         //Input weighted sum
         h_out[0][i] = 0;
-        for (int j = 0; j < INPUTS; j++) {
+        for (j = 0; j < INPUTS; j++) {
             h_out[0][i] += weights_in[(i * INPUTS) + j] * in[j];
         }
-        
+
         //Bias weighted sum
         #if (BIAS > 0)
         h_out[0][i] += weights_bias_h[0][i] * BIAS;
         #endif
-        
+
         //Activation
         h_out[0][i] = activation(h_out[0][i]);
         //printf("h_out[0][%d]: %f\n", i, h_out[0][i]);
     }
     //------------------------------------
-    
+
     //Hidden Layers----------------------------
     #if (H_LAYERS > 1)
-    for (int i = 0; i < (H_LAYERS - 1); i++) { //For each layer
-        for (int j = 0; j < H_HEIGHT; j++) { //For each node in a layer
+    for (i = 0; i < (H_LAYERS - 1); i++) { //For each layer
+        for (j = 0; j < H_HEIGHT; j++) { //For each node in a layer
             //Hidden weighted sum
             h_out[i+1][j] = 0;
-            for (int k = 0; k < H_HEIGHT; k++) { //For each node in previous layer
+            for (k = 0; k < H_HEIGHT; k++) { //For each node in previous layer
                 h_out[i+1][j] += weights_h[i][(j * H_HEIGHT) + k] * h_out[i][k];
             }
-            
+
             //Bias weighted sum
             #if (BIAS > 0)
             h_out[i+1][j] += weights_bias_h[i+1][j] * BIAS;
             #endif
-            
+
             //Activation
             h_out[i+1][j] = activation(h_out[i+1][j]);
-            printf("h_out[%d][%d]: %f\n", i+1, j, h_out[i+1][j]);
+            //printf("h_out[%d][%d]: %f\n", i+1, j, h_out[i+1][j]);
         }
     }
     #endif
     //-----------------------------------------
-    
+
     //Output Layer---------------------
-    for (int i = 0; i < OUTPUTS; i++) {
+    for (i = 0; i < OUTPUTS; i++) {
         //Hidden layer weighted sum
         outputs[i] = 0;
-        for (int j = 0; j < H_HEIGHT; j++) {
+        for (j = 0; j < H_HEIGHT; j++) {
             outputs[i] += weights_out[(i * OUTPUTS) + j] * h_out[H_LAYERS - 1][j];
         }
-        
+
         //Bias weighted sum
         #if (BIAS > 0)
         outputs[i] += weights_bias_out[i] * BIAS;
         #endif
 
         //Normalize output
-        outputs[i] = normalize(outputs[i]);
+        //outputs[i] = normalize(outputs[i]);
 
         //Activation
         //outputs[i] = activation(outputs[i]);
@@ -145,21 +148,22 @@ void forward(float in[INPUTS]) {
     //-----------------------------------
 }
 
-void backpropagation(float in[INPUTS], float target_out[OUTPUTS]) {
-    float delta_sum;
-    float deltas_h[H_HEIGHT];
-    float deltas_o[OUTPUTS];
-    float weights_out_new[OUTPUTS * H_HEIGHT];
+void backpropagation(double in[INPUTS], double target_out[OUTPUTS]) {
+    int i,j,k;
+    double delta_sum;
+    double deltas_h[H_HEIGHT];
+    double deltas_o[OUTPUTS];
+    double weights_out_new[OUTPUTS * H_HEIGHT];
     #if (H_LAYERS > 1)
     //TODO: Make this more memory efficient
-    float weights_h_new[H_LAYERS - 1][H_HEIGHT * H_HEIGHT];
+    double weights_h_new[H_LAYERS - 1][H_HEIGHT * H_HEIGHT];
     #endif
 
-    
+
     //Output layer------------------------------
-    for (int i = 0; i < OUTPUTS; i++) {
-        deltas_o[i] = (outputs[i] - target_out[i]);// * (outputs[i] * (1 - outputs[i]));
-        for (int j = 0; j < H_HEIGHT; j++) {
+    for (i = 0; i < OUTPUTS; i++) {
+        deltas_o[i] = (outputs[i] - target_out[i]);
+        for (j = 0; j < H_HEIGHT; j++) {
             weights_out_new[(i * OUTPUTS) + j] = weights_out[(i * OUTPUTS) + j] -
                     (ALPHA * deltas_o[i] * h_out[H_LAYERS - 1][j]);
             //printf("New weight_out[%d]: %f\n", (i * OUTPUTS) + j, weights_out_new[(i * OUTPUTS) + j]);
@@ -172,65 +176,60 @@ void backpropagation(float in[INPUTS], float target_out[OUTPUTS]) {
 
     //Hidden layers--------------------------------------------------------------------------------
     #if (H_LAYERS > 1)
-    //TODO: Output Layer
     //Layer connected to outputs
-    for (int i = 0; i < H_HEIGHT; i++) { //For each node in hidden layer
+    for (i = 0; i < H_HEIGHT; i++) { //For each node in hidden layer
         delta_sum = 0;
-        for (int j = 0; j < OUTPUTS; j++) {  //For each output
-            delta_sum += weights_out[i + (j * OUTPUTS)] * deltas_o[j];
+        for (j = 0; j < OUTPUTS; j++) {  //For each output
+            delta_sum += weights_out[i + (j * H_HEIGHT)] * deltas_o[j];
         }
         deltas_h[i] = (h_out[H_LAYERS-1][i] * (1 - h_out[H_LAYERS-1][i])) * delta_sum;
-        for (int j = 0; j < H_HEIGHT; j++) {
-            weights_h_new[H_LAYERS-1][(i * H_HEIGHT) + j] = weights_h[H_LAYERS-1][(i * H_HEIGHT) + j] -
+        for (j = 0; j < H_HEIGHT; j++) {
+            weights_h_new[H_LAYERS-2][(i * H_HEIGHT) + j] = weights_h[H_LAYERS-2][(i * H_HEIGHT) + j] -
                     (ALPHA * deltas_h[i] * h_out[H_LAYERS-2][j]);
-            printf("New weight_h[%d][%d]: %f\n", H_LAYERS-2, (i * H_HEIGHT) + j, weights_h_new[H_LAYERS-2][(i * H_HEIGHT) + j]);
         }
         #if (BIAS > 0)
-        weights_bias_h[H_LAYERS-2][i] -= ALPHA * deltas_h[i] * BIAS;
+        weights_bias_h[H_LAYERS-1][i] -= ALPHA * deltas_h[i] * BIAS;
         #endif
     }
-    //TODO: Hidden Layers
     //Each hidden layer not connected to input/hidden output layer
-    for (int i = H_LAYERS-2; i > 1; i--) {
-        for (int j = 0; j < H_HEIGHT; j++) {
+    for (i = H_LAYERS-2; i > 0; i--) {
+        for (j = 0; j < H_HEIGHT; j++) {
             delta_sum = 0;
-            for (int k = 0; k < OUTPUTS; k++) {
-                delta_sum += weights_out[i + (k * OUTPUTS)] * deltas_o[k];
+            for (k = 0; k < H_HEIGHT; k++) {
+                delta_sum += weights_h[i][j + (k * H_HEIGHT)] * deltas_h[k];
             }
-            deltas_h[i] = (h_out[0][i] * (1 - h_out[0][i])) * delta_sum;
-            for (int k = 0; k < INPUTS; k++) {
-                weights_in[(i * INPUTS) + k] -= (ALPHA * deltas_h[i] * in[k]);
-                printf("New weight_h[%d][%d]: %f\n", , , );
+            deltas_h[j] = (h_out[i][j] * (1 - h_out[i][j])) * delta_sum;
+            for (k = 0; k < H_HEIGHT; k++) {
+                weights_h_new[i-1][(j * H_HEIGHT) + k] = weights_h[i-1][(j * H_HEIGHT) + k] -
+                        (ALPHA * deltas_h[j] * h_out[i - 1][k]);
             }
             #if (BIAS > 0)
-            weights_bias_h[0][i] -= ALPHA * deltas_h[i] * BIAS;
+            weights_bias_h[i][j] -= ALPHA * deltas_h[j] * BIAS;
             #endif
         }
     }
-    //TODO: Input Layer
     //Layer connected to inputs
-    for (int i = 0; i < H_HEIGHT; i++) {
+    for (i = 0; i < H_HEIGHT; i++) {
         delta_sum = 0;
-        for (int j = 0; j < OUTPUTS; j++) {
-            delta_sum += weights_out[i + (j * OUTPUTS)] * deltas_o[j];
+        for (j = 0; j < H_HEIGHT; j++) {
+            delta_sum += weights_h[0][i + (j * H_HEIGHT)] * deltas_h[j];
         }
         deltas_h[i] = (h_out[0][i] * (1 - h_out[0][i])) * delta_sum;
-        for (int j = 0; j < INPUTS; j++) {
+        for (j = 0; j < INPUTS; j++) {
             weights_in[(i * INPUTS) + j] -= (ALPHA * deltas_h[i] * in[j]);
-            printf("New weight_in[%d]: %f\n", (i * INPUTS) + j, weights_in[(i * INPUTS) + j]);
         }
         #if (BIAS > 0)
         weights_bias_h[0][i] -= ALPHA * deltas_h[i] * BIAS;
         #endif
     }
     #else
-    for (int i = 0; i < H_HEIGHT; i++) {
+    for (i = 0; i < H_HEIGHT; i++) {
         delta_sum = 0;
-        for (int j = 0; j < OUTPUTS; j++) {
+        for (j = 0; j < OUTPUTS; j++) {
             delta_sum += weights_out[i + (j * OUTPUTS)] * deltas_o[j];
         }
         deltas_h[i] = (h_out[0][i] * (1 - h_out[0][i])) * delta_sum;
-        for (int j = 0; j < INPUTS; j++) {
+        for (j = 0; j < INPUTS; j++) {
             weights_in[(i * INPUTS) + j] -= (ALPHA * deltas_h[i] * in[j]);
             //printf("New weight_in[%d]: %f\n", (i * INPUTS) + j, weights_in[(i * INPUTS) + j]);
         }
@@ -243,7 +242,7 @@ void backpropagation(float in[INPUTS], float target_out[OUTPUTS]) {
 
     //Write new values---------------------------
     //Output layer
-    for (int i = 0; i < OUTPUTS * H_HEIGHT; i++) {
+    for (i = 0; i < OUTPUTS * H_HEIGHT; i++) {
         weights_out[i] = weights_out_new[i];
     }
     //Hidden layer
@@ -257,31 +256,50 @@ void backpropagation(float in[INPUTS], float target_out[OUTPUTS]) {
     //--------------------------------------------
 }
 
+void print_weights() {
 
+}
 
 int main() {
-    float training_in[10], training_out[10];
-    for (int i = 0; i < 10; i++) {
-        training_in[i] = normalize(i + 2);
-        training_out[i] = normalize((i + 2) * (i + 2));
+    int i,j;
+    unsigned long int it = 0;
+    struct timeval t1, t2;
+//    double training_in[] = {0.1};
+//    double training_out[] = {0.9};
+    double error;
+    double training_in[5], training_out[5];
+    for (i = 0; i < 10; i++) {
+        training_in[i] = normalize(11-i);
+        training_out[i] = normalize((11-i) * (11-i));
     }
 
     initialize();
 
-    for (int i = 0; i < 1000000; i++) {
-        for (int j = 0; j < 10; j++) {
+
+    gettimeofday(&t1, NULL);
+    error = 1;
+//    for (i = 0; i < 1000000; i++) {
+    while (error > 1E-4) {
+        error = 0;
+        for (j = 0; j < 10; j++) {
             forward(&training_in[j]);
+            error += (training_out[j] - outputs[0]) * (training_out[j] - outputs[0]);
+//            printf("%2.0f^2 = %f\n", denormalize(training_in[j]), denormalize(outputs[0]));
             backpropagation(&training_in[j], &training_out[j]);
         }
+        printf("Iteration:%lu  Error:%12.10f\n", it++, error);
     }
+    gettimeofday(&t2, NULL);
+    printf("Training time: %f\n", t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec)*1.0E-6);
+    printf("Iterations: %lu\n", it);
 
-    float error = 0;
-    for (int i = 0; i < 10; i++) {
+    error = 0;
+    for (i = 0; i < 10; i++) {
         forward(&training_in[i]);
         error += (training_out[i] - outputs[0]) * (training_out[i] - outputs[0]);
-        printf("%.0f^2 = %f\n", denormalize(training_in[i]), denormalize(outputs[0]));
+        printf("%2.0f^2 = %f\n", denormalize(training_in[i]), denormalize(outputs[0]));
     }
-    printf("Total Error: %f\n", error);
+    printf("Total Error: %.3e\n", error);
     
     
     return 0;
