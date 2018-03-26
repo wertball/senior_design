ce7c71c329d6b37b9463e532659d721887ec5542
BKS0015 <bks0015@auburn.edu>
2018-03-25 22:44:49 -0500

    this commit bring's my version of the nn in line with thomas' and adds a couple of bells/whistles to the main file. Also, weights are initialized to values between [-1:1] in order to achieve a more constant test error.

--- brandon_nn/main.c
+++ brandon_nn/main.c
@@ -1,87 +1,66 @@
 #include "neural_net.h"
-#include "debug.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
-#include <math.h>
+#include <errno.h>
 
+//Flags---------------------------------------------------------
+#define DEBUGGING //comment this flag to disable debug messages
+#define FILE_IO   //comment this flag to disable file IO
+
+#ifdef DEBUGGING
+# define DEBUG(x) x
+#else
+# define DEBUG(x) do {} while (0)
+#endif
+
+#ifdef FILE_IO
+# define FIO(x) x
+#else
+# define FIO(x) do {} while (0)
+#endif
+//--------------------------------------------------------------
+
+//Network Parameters--------------
 #define num_layers 5
-#define lc 0.2
-#define training_iterations 1e7
-#define target_error 1e-8
-#define data_min 0.549361
+#define learning_rate 0.2
+#define training_iterations 1e4
+#define target_error 5e-2
+#define data_min 0.520171
 #define data_max 3.43917
-#define data_range (data_max - data_min)
 
 #define training_set_size 55
 #define set_input_size 5
 #define testing_set_size 360
-#define input_set_lower_bound 0.1
-#define input_set_upper_bound 0.5
-#define input_set_step_size 0.1
+//Network Parameters--------------
 
-calc_t normalize(calc_t input){
-	return (input - data_min) / data_range;
-}
+//File Locations------------------------------------------------------------------------
+#define file_base_directory "F:\\school\\ELEC4000\\senior_design\\gitRepo\\thomas_nn\\"
+#define test_output_file (file_base_directory"test_output.txt")
+#define error_results_file (file_base_directory"Error_Results.txt")
+#define test_results_file (file_base_directory"Test_Results.txt")
+//--------------------------------------------------------------------------------------
 
-calc_t denormalize(calc_t input){
-	return data_range * input + data_min;
-}
+int main(void){
 
- int main(void){
-    FILE *fp;
+	FILE *fp;
 
 	//network initialization parameters
-    uint8_t dim[num_layers] = {
-            set_input_size,  //layer 1, input layer
-            20,              //layer 2, hidden layer
+	uint8_t dim[num_layers] = {
+			set_input_size,  //layer 1, input layer
+			20,              //layer 2, hidden layer
             20,              //layer 3, hidden layer
             20,              //layer 4, hidden layer
 			//5,				//layer 5, hidden layer
-            1               //layer 6, output layer
-    };	//number of nodes in each layer
-    Neural_Net_Init_Params *nnip = &(Neural_Net_Init_Params){num_layers, dim, lc};
+			1               //layer 6, output layer
+	};	//number of nodes in each layer
+	Neural_Net_Init_Params *nnip = &(Neural_Net_Init_Params){num_layers, dim, learning_rate};
 
-    //create and initialize neural net
-    Neural_Net *nn = init_neural_net(nnip);
-    printWeights(nn);
+	//create and initialize neural net
+	Neural_Net *nn = init_neural_net(nnip);
+	DEBUG(printWeights(nn););
 
-    //create input and output arrays, print during debug
-    Input_Set_Params *isp = &(Input_Set_Params) {
-             training_set_size,         //number of input sets
-             set_input_size,            //number of elements in a single input set
-             input_set_lower_bound,     //minimum value of input set range
-             input_set_upper_bound,     //maximum value of input set range
-             input_set_step_size        //step size between min and max values
-    };
-    //calc_t** input_sets = init_inputs(isp);
-//    calc_t denormalized_input_sets[training_set_size][set_input_size] = {
-//            {5, 5},
-//            {5, 4},
-//            {5, 3},
-//            {5, 2},
-//            {5, 1},
-//            {4, 5},
-//            {4, 4},
-//            {4, 3},
-//            {4, 2},
-//            {4, 1},
-//            {3, 5},
-//            {3, 4},
-//            {3, 3},
-//            {3, 2},
-//            {3, 1},
-//            {2, 5},
-//            {2, 4},
-//            {2, 3},
-//            {2, 2},
-//            {2, 1},
-//            {1, 5},
-//            {1, 4},
-//            {1, 3},
-//            {1, 2},
-//            {1, 1}
-//    };
+	//Create input data--------------------------------------------------------------------
 	calc_t denormalized_input_sets[training_set_size][set_input_size];
 	for(int i = 0; i < training_set_size; i++){
 		denormalized_input_sets[i][0] = (calc_t) 10.0;
@@ -90,7 +69,9 @@ calc_t denormalize(calc_t input){
 		denormalized_input_sets[i][3] = (calc_t) ((i / 11) * 10.0 + 80.0);
 		denormalized_input_sets[i][4] = (calc_t) (1.79 + (i % 11)/100.0);
 	}
+	//-------------------------------------------------------------------------------------
 
+	//Normalize input data-----------------------------------------------------------------
 	calc_t input_sets[training_set_size][set_input_size];
 	for(int i = 0; i < training_set_size; i++){
 		input_sets[i][0] = denormalized_input_sets[i][0] / 10.0;
@@ -101,59 +82,67 @@ calc_t denormalize(calc_t input){
 		//for(int j = 0; j < set_input_size; j++i)
 		//	input_sets[i][j] = normalize(denormalized_input_sets[i][j]);
 	}
+	//-------------------------------------------------------------------------------------
 
+	//Create output data--------------------------------------------------------------------
 	calc_t denormalized_output[training_set_size] = {
-		3.43313,
-		2.71998,
-		2.23442,
-		1.87542,
-		1.57747,
-		1.32098,
-		1.10156,
-		0.921271,
-		0.782483,
-		0.681796,
-		0.610265,
-		3.43917,
-		2.78111,
-		2.25032,
-		1.86824,
-		1.55482,
-		1.30133,
-		1.07933,
-		0.897866,
-		0.75942,
-		0.658494,
-		0.587405,
-		2.98477,
-		2.49611,
-		2.12776,
-		1.78697,
-		1.49209,
-		1.2415,
-		1.02859,
-		0.858088,
-		0.727433,
-		0.633834,
-		0.566431,
-		3.30783,
-		2.64033,
-		2.13554,
-		1.78793,
-		1.5096,
-		1.25894,
-		1.04158,
-		0.865621,
-		0.731832,
-		0.635492,
-		0.567038,
-		2.901,
-		2.35628,
-		1.98702,
-		1.70146,
-		1.43822,
-		1.19831
-		};
+			3.43313,
+			2.71998,
+			2.23442,
+			1.87542,
+			1.57747,
+			1.32098,
+			1.10156,
+			0.921271,
+			0.782483,
+			0.681796,
+			0.610265,
+			3.43917,
+			2.78111,
+			2.25032,
+			1.86824,
+			1.55482,
+			1.30133,
+			1.07933,
+			0.897866,
+			0.75942,
+			0.658494,
+			0.587405,
+			2.98477,
+			2.49611,
+			2.12776,
+			1.78697,
+			1.49209,
+			1.2415,
+			1.02859,
+			0.858088,
+			0.727433,
+			0.633834,
+			0.566431,
+			3.30783,
+			2.64033,
+			2.13554,
+			1.78793,
+			1.5096,
+			1.25894,
+			1.04158,
+			0.865621,
+			0.731832,
+			0.635492,
+			0.567038,
+			2.901,
+			2.35628,
+			1.98702,
+			1.70146,
+			1.43822,
+			1.19831,
+			0.992931,
+			0.826196,
+			0.701636,
+			0.612685,
+			0.549361
+	};
+	//--------------------------------------------------------------------------------------
 
 	calc_t denormalized_test_input_sets[testing_set_size][set_input_size];
 	for(int i = 0; i < testing_set_size; i++){
@@ -164,6 +153,7 @@ calc_t denormalize(calc_t input){
 		denormalized_test_input_sets[i][4] = (calc_t) (1.795 + (i % 10)/100.0);
 	}
 
+	//Normalize output data-----------------------------------------------------------------
 	calc_t test_input_sets[testing_set_size][set_input_size];
 	for(int i = 0; i < testing_set_size; i++){
 		test_input_sets[i][0] = denormalized_test_input_sets[i][0] / 10.0;
@@ -172,93 +162,99 @@ calc_t denormalize(calc_t input){
 		test_input_sets[i][3] = (denormalized_test_input_sets[i][3] - 80.0) / (120.0 - 80.0);
 		test_input_sets[i][4] = (denormalized_test_input_sets[i][4] - 1.79) / (1.89 - 1.79);
 	}
+	//--------------------------------------------------------------------------------------
 
 	float t0;
 	float denormalized_test_output[testing_set_size];
-	fp = fopen("test_output.txt","r");
+
+	fp = fopen(test_output_file,"r");
+	if(fp == NULL)
+		printf("Failed to read %s\nerrno: %d\n", test_output_file, errno);
 	for(int i = 0; i < testing_set_size; i++){
 		fscanf(fp, "%f", &t0);
 		denormalized_test_output[i] = t0;
 	}
-	
 	fclose(fp);
 
 	calc_t test_output[testing_set_size];
 	for(int i = 0; i < testing_set_size; i++){
-		test_output[i] = normalize((calc_t)(denormalized_test_output[i]));
+		test_output[i] = normalize((calc_t)(denormalized_test_output[i]), data_min, data_max);
 	}
 
 	calc_t output[training_set_size];
 	for(int i = 0; i < training_set_size; i++){
-		output[i] = normalize(denormalized_output[i]);
+		output[i] = normalize(denormalized_output[i], data_min, data_max);
 	}
 
-	fp = fopen("Error_Results.txt","w+");
 
-    //init_inputs(~)
-    //init_outputs(~)
-    //printInputs(isp, input_sets);
-    //printOutputs(isp, output);
-
-    //train it
-    int i;
-    struct timeval t1, t2;
-	//fprintf(fp,"Iterations\t\t\tError\n");
-    gettimeofday(&t1, NULL);
-    calc_t error = 1.0;
+	//train the network
+	int i, j;
+	struct timeval t1, t2;
+	gettimeofday(&t1, NULL);
+	calc_t error = 1.0;
 	calc_t error_1 = 0.0;
-    for(i = 0; i < training_iterations && error > target_error; i++) {
-        error = 0.0;
-		//fprintf(fp,"%d: ",i);
-		for(int j = 0; j < training_set_size; j++) {
-            error_1 = train_network(nn, input_sets[j], output[j]);
-			//fprintf(fp,"%d\t%.2e\n",i*training_set_size+j,error_1);
+    fp = fopen(error_results_file,"w");
+    if(fp == NULL)
+        printf("Failed to write %s\nerrno: %d\n", error_results_file, errno);
+
+	//train using percent error calculation--------------------------
+	for(i = 0; i < training_iterations && error > target_error; i++){
+		error = 0.0;
+        //assume update weights to be zero
+		for(j = 0; j < training_set_size; j++){
+			//training
+			feed_forward(nn,input_sets[j]);
+			nn->cte = find_total_error(output[j], nn->layer[nn->l-1]->node[0]->o);
+			backpropagate(nn, output[j]);
+			//update_weights(nn);
+			//determining percent error
+			error_1 = percent_error(output[j], nn->layer[nn->l-1]->node[0]->o);
+			//printf("%d\nparameters: %f, %f\n", j, output[j], nn->layer[nn->l-1]->node[0]->o);
+			//printf("error_1: %f\n", error_1);
 			error += error_1;
-        }
-		error = error/training_set_size;
+		}
+        //update_weights(nn, training_set_size)
+		error /= training_set_size;
 		fprintf(fp,"%d\t%.2e\n",i,error);
-		//fprintf(fp,"%.2e\n",error);
-    }
-    gettimeofday(&t2, NULL);
+	}
+	//---------------------------------------------------------------
+
+    //record and print training results------------------------------------------------------------
+	gettimeofday(&t2, NULL);
 	fclose(fp);
-    printf("execution time(s): %.4f\n", t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec)*1.0E-6);
-    printf("total error: %e\n", error);
-    printf("total runs = %d\n", i);
+	printf("execution time(s): %.4f\n", t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec)*1.0E-6);
+	printf("total error: %e\n", error);
+	printf("total runs = %d\n", i);
+    //---------------------------------------------------------------------------------------------
 
-	fopen("Test_Results.txt","w+");
 
-	float difference;
-	float test_error = 0;
+	//test network
+    calc_t test_error = 0;
+    calc_t largest = 0.0;
+    calc_t smallest = 100.0;
+    fp = fopen(test_results_file,"w");
+    if(fp == NULL)
+        printf("Failed to write %s\nerrno: %d", test_results_file, errno);
 
 	for(i = 0; i < testing_set_size; i++){
 		feed_forward(nn, test_input_sets[i]);
 		calc_t out = nn->layer[num_layers - 1]->node[0]->o;
-        fprintf(fp,"%.2f, %.2f, ", denormalize(out), denormalized_test_output[i]);
-		difference = denormalized_test_output[i] - denormalize(out);
-		fprintf(fp,"Error = %.2f%\n", difference);
-		test_error += difference;
+		fprintf(fp,"%.2f, %.2f, ", denormalize(out, data_min, data_max), denormalized_test_output[i]);
+		error = percent_error(denormalized_test_output[i], denormalize(out, data_min, data_max));
+		fprintf(fp,"Error = %.2f\n", error);
+		test_error += error;
+        smallest = (error < smallest) ? error : smallest;
+        largest = (error > largest) ? error : largest;
 	}
 	fclose(fp);
-	printf("Test Error: %e\n", test_error/testing_set_size);
+    printf("Test Error: \n");
+    printf("smallest error: %.4f\n", smallest);
+    printf("largest error: %.4f\n", largest);
+    printf("range of errors: %.4f\n", largest - smallest);
+	printf("average error: %.2f\n", test_error/testing_set_size);
 
-    //test network
-//    calc_t input_set[isp->input_set_size];
-//    calc_t x = 0.5;
-//    while(x <= 8.5){
-//        calc_t y = 0.5;
-//        while(y <= 8.5){
-//            input_set[0] = normalize(x);
-//            input_set[1] = normalize(y);
-//            feed_forward(nn, input_set);
-//            calc_t out = nn->layer[num_layers - 1]->node[0]->o;
-//            fprintf(fp,"Input: (%.2f, %.2f) => %.2f, %.2f, ", x, y, denormalize(out), ((x-5)*(x-5)+(y-5)*(y-5)));
-//            fprintf(fp,"Error = %.2e%\n", percent_error((x-5)*(x-5)+(y-5)*(y-5), denormalize(out)));
-//            y += 0.5;
-//        }
-//        x += 0.5;
-//    }
-//	fclose(fp);
-    free(nn); //free(input_sets); free(output);
-    return 0;
+	//dealloc neural network
+	//dealloc(nn);
+	return 0;
 }
 
--- brandon_nn/neural_net.c
+++ brandon_nn/neural_net.c
@@ -1,5 +1,4 @@
 #include "neural_net.h"
-#include "debug.h"
 #include <stdlib.h>
 #include <math.h>
 #include <sys/time.h>
@@ -21,9 +20,6 @@ Neural_Net* init_neural_net(Neural_Net_Init_Params *nnip){
     nn->nw = 0;             //set number of weights to 0 as prep for running sum
     nn->lc  = nnip->lc;     //set learning constant
     nn->cte = 1.0;          //set current total error
-	//nn->data_min = nnip->data_min;
-	//nn->data_max = nnip->data_max;
-	//nn->data_range = nnip->data_range;
     //allocate layer pointer array memory
     nn->layer = malloc(sizeof(nn->layer) * nn->l);
 
@@ -56,7 +52,7 @@ Neural_Net* init_neural_net(Neural_Net_Init_Params *nnip){
         n->nw = nnip->dim[1];           //set number of weights to number of nodes in next layer
         n->o = 0;                       //set output of node to 0
         n->d = 0;                       //set delta of node to 0
-        n->cw = init_weights(n->nw);    //cw = nw size array of random weights
+        n->cw = init_weights(n->nw, 1); //cw = nw size array of random weights
         n->uw = n->cw;                  //uw = cw
     }
 
@@ -74,12 +70,12 @@ Neural_Net* init_neural_net(Neural_Net_Init_Params *nnip){
         nn->nn += l->nn;    //increment number of nodes in neural net
         //node parameters
         for (j = 0; j < l->nn; j++) {
-            n = l->node[j];                 //current node base address
-            n->nw = nnip->dim[i + 1];       //set number of weights to number of nodes in next layer
-            n->o = 1;                       //set output of node to 0
-            n->d = 3;                       //set delta of node to 0
-            n->cw = init_weights(n->nw);    //cw = nw size array of random weights
-            n->uw = n->cw;                  //uw = cw
+            n = l->node[j];                    //current node base address
+            n->nw = nnip->dim[i + 1];          //set number of weights to number of nodes in next layer
+            n->o = 1;                          //set output of node to 0
+            n->d = 3;                          //set delta of node to 0
+            n->cw = init_weights(n->nw, 1);    //cw = nw size array of random weights
+            n->uw = n->cw;                     //uw = cw
         }
     }
 
@@ -105,24 +101,22 @@ Neural_Net* init_neural_net(Neural_Net_Init_Params *nnip){
     return nn;
 }
 
-calc_t* init_weights(int size){
+calc_t* init_weights(int size, calc_t bound){
     //allocate memory for weight array
     calc_t* arr = malloc(sizeof(*arr) * size);
     //randomize array with values between range [0, upper bound]
-    int upper_bound = 5;
     for(int i = 0; i < size; i++){
         calc_t sum = 0;
         if(rand() >= RAND_MAX / 2)
-            sum += (calc_t)rand()/(calc_t)(RAND_MAX/upper_bound);
+            sum += (calc_t)rand()/(calc_t)(RAND_MAX/bound);
         else
-            sum -= (calc_t)rand()/(calc_t)(RAND_MAX/upper_bound);
+            sum -= (calc_t)rand()/(calc_t)(RAND_MAX/bound);
         arr[i] = sum;
     }
     return arr;
 }
 
 calc_t train_network(Neural_Net *nn, calc_t *input, calc_t output){
-
     //(1) feed forward
     feed_forward(nn, input);
     //(2) calculate total error output
@@ -132,7 +126,7 @@ calc_t train_network(Neural_Net *nn, calc_t *input, calc_t output){
     //(4) assign new weights
     update_weights(nn);
 
-	return nn->cte;
+    return nn->cte;
 }
 
 void feed_forward(Neural_Net *nn, calc_t *input){
@@ -142,6 +136,7 @@ void feed_forward(Neural_Net *nn, calc_t *input){
 
     //-----------------------input layer----------------------
     l = nn->layer[0];
+#pragma omp parallel for private(i)
     for(i = 0; i < l->nn; i++){
         l->node[i]->o = input[i];
     }
@@ -150,6 +145,7 @@ void feed_forward(Neural_Net *nn, calc_t *input){
     for(i = 1; i < (nn->l - 1); i++){
         l = nn->layer[i];
         pl = l->prev;
+#pragma omp parallel for private(j,k,sum)
         for(j = 0; j < l->nn; j++){
             for(sum = 0, k = 0; k < pl->nn; k++)
                 sum +=  pl->node[k]->o * pl->node[k]->cw[j];
@@ -160,10 +156,11 @@ void feed_forward(Neural_Net *nn, calc_t *input){
     //-----------------------output layer----------------------
     l = nn->layer[nn->l-1];
     pl = l->prev;
+#pragma omp parallel for private(j,k,sum)
     for(j = 0; j < l->nn; j++){
         for(sum = 0, k = 0; k < pl->nn; k++)
             sum +=  pl->node[k]->o * pl->node[k]->cw[j];
-        l->node[j]->o = nn_normalize(sum);
+        l->node[j]->o = normalize(sum, 0.549361, 3.43917);
     }
 }
 
@@ -173,12 +170,12 @@ calc_t find_total_error(calc_t desired, calc_t actual){
 
 void backpropagate(Neural_Net *nn, calc_t output) {
     calc_t dw,          //dE/dw,
-          delta,        //dE/dni
-          lc = nn->lc;  //learning constant
+            delta,       //dE/dni
+            lc = nn->lc; //learning constant
     int i, j;   //iterators
     Layer *pl,  //previous layer pointer
-          *cl,  //current layer pointer
-          *nl;  //next layer pointer
+            *cl,  //current layer pointer
+            *nl;  //next layer pointer
 
     //-------------------------output layer------------------------------
     cl = nn->layer[nn->l - 1];      //current layer = output layer
@@ -188,6 +185,7 @@ void backpropagate(Neural_Net *nn, calc_t output) {
         delta = (cl->node[i]->o - output) * (1); //delta = (dE/dno)(dno/dni)
         cl->node[i]->d = delta;
         //update previous layer nodes
+#pragma omp parallel for private(j,dw)
         for (j = 0; j < pl->nn; j++) {              //for every node in previous layer
             Node *pln = pl->node[j];
             dw = delta * pln->o;                    //dw = (delta)(node output)
@@ -240,31 +238,25 @@ calc_t percent_error(calc_t desired, calc_t actual){
     return fabs((actual - desired) / desired * (calc_t)100.0);
 }
 
-calc_t nn_normalize(calc_t input){
-	return (input - 0.549361) / (3.43917 - 0.549361);
+calc_t normalize(calc_t input, calc_t min, calc_t max){
+    return (input - min) / (max - min);
 }
 
-calc_t* init_outputs(calc_t** input_set, uint8_t set_size){
-    //manually implement f(x)
-    calc_t *output = malloc(sizeof(*output) * set_size);
-    for(int i = 0; i < set_size; i++){
-        calc_t input_x = input_set[i][0];
-        calc_t input_y = input_set[i][1];
-        output[i] = (calc_t)sqrt(input_x + input_y); //y = (x+y)^1/2
-    }
-    return output;
+calc_t denormalize(calc_t input, calc_t min, calc_t max){
+    return (max - min) * input + min;
 }
 
-calc_t** init_inputs(Input_Set_Params *isp){
-    calc_t** inputs = malloc(sizeof(*inputs) * isp->input_set_size);
-    for(int i = 0; i < isp->set_size; i++){
-        calc_t *input_set = malloc(sizeof(*input_set) * isp->input_set_size);
-        calc_t input = isp->min;
-        for(int j = 0; j < isp->input_set_size; j++){
-            input_set[j] = input;
-            input += isp->step_size;
+//help functions
+void printWeights(Neural_Net *nn){
+    printf("Listing Weights ----------------------------------------------------------------\n");
+    for(int i = 0; i < nn->l; i++){
+        printf("Layer[%d]:\n", i);
+        for(int j = 0; j < nn->layer[i]->nn; j++){
+            printf("\tNode[%d]:\n", j);
+            for(int k = 0; k < nn->layer[i]->node[j]->nw; k++){
+                printf("\t\tWeight[%d]: %f\n", k, nn->layer[i]->node[j]->cw[k]);
+            }
         }
-        inputs[i] = input_set;
     }
-    return inputs;
+    printf("--------------------------------------------------------------------------------\n");
 }
